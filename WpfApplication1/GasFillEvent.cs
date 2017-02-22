7a00e225c6119c298679a455ce0046527e0df5a2
Éric Daigneault <daigno@gmail.com>
2017-02-22 16:19:01 -0500

    just started, pushing to git

---
+++ WpfApplication1/GasFillEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLog
+{
+    class GasFillEvent
+    {
+        public int TotalPrice { get; private set; }
+        public int TotalLiters { get; private set; }
+        public int PricePerLiters { get; private set; }
+    }
+}
